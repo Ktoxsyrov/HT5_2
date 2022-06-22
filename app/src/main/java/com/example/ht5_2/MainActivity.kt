e9eff74fc004c7df847f2129dbe7e0ab3a054cd7
Ktoxsyrov <ktoxsyrovv@inbox.ru>
2022-06-22 13:51:29 +0300

    init

--- app/src/main/java/com/example/ht5_2/MainActivity.kt
+++ app/src/main/java/com/example/ht5_2/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity(){
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         adapter = SuperHeroesAdapter()
         binding.heroesRecyclerView.addOnItemCLickListener(object : OnItemClickListener {
             override fun onItemClicked(position: Int, view: View) {
